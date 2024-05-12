a569a95b9fea80c74e34a546e3f4624aeb427eba
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-05-12 18:41:03 -0300

    Project created

---
+++ WpfClientNamedPipe/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClientNamedPipe
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private NamedPipeClientStream _pipeClient;
+    private Process _serverProcess;
+    private readonly string pathExe = @"C:\Users\gabri\OneDrive\Documentos\workspaces\vs\NamedPipeWPF\WpfServerNamedPipe\bin\Debug\net6.0-windows\WpfServerNamedPipe.exe";
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private async void Connection(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        //iniciar processo
+        _serverProcess = new Process();
+        _serverProcess.StartInfo.FileName = pathExe;
+        _serverProcess.Start();
+
+        //espera 5 segundos para conexão
+        //await Task.Delay(5000);
+
+        _pipeClient = new(".", "TestPipe", PipeDirection.InOut, PipeOptions.None);
+        _pipeClient.Connect();
+        StreamWriter writer = new(_pipeClient);
+        writer.WriteLine("Gabriel");
+        writer.Flush();
+
+        StreamReader reader = new StreamReader(_pipeClient);
+        string response = reader.ReadToEnd();
+        //MessageBox.Show(response);
+
+        StatusText.Text = response;
+
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("Erro ao Conectar:" + ex.Message);
+      }
+    }
+  }
+}
---
+++ WpfServerNamedPipe/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.IO.Pipes;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfServerNamedPipe
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private NamedPipeServerStream _pipeServer;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      StartServer();
+    }
+
+    private void StartServer()
+    {
+      Task.Run(() =>
+      {
+        try
+        {
+          _pipeServer = new NamedPipeServerStream("TestPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+          _pipeServer.WaitForConnection();
+
+          StreamReader reader = new StreamReader(_pipeServer);
+          string? name = reader.ReadLine();
+
+          if (name == "Protocol" || !String.IsNullOrWhiteSpace(name))
+          {
+            Dispatcher.Invoke(() => StatusText.Text = name);
+            StreamWriter writer = new StreamWriter(_pipeServer);
+            writer.WriteLine($"Olá {name}! sou o Servidor WPF!");
+            writer.Flush();
+          }
+          _pipeServer.Disconnect();
+          Application.Current.Dispatcher.Invoke(() => Application.Current.Shutdown());
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show("Erro ao Conectar:" + ex.Message);
+        }
+        finally
+        {
+
+
+        }
+      });
+    }
+  }
+}
